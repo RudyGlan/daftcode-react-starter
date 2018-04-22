bcef5cc98be929b3bc509c36c9c0396098c4ed16
Alicja Pełka <alicjapraczukowska@gmail.com>
2018-04-23 00:13:40 +0200

    counter update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,57 +3,57 @@ import PropTypes from "prop-types";
 
 class Counter extends React.Component {
   
-    constructor(){
-      super();
-      this.state = {timeDisplay: {seconds: 0, minutes: 0}}
-    }
 
     static propTypes = {
       fromNum: PropTypes.number.isRequired,
       toNum: PropTypes.number.isRequired,
     }
-  
-    getInitialState() {
-      return {
-        timeDisplay: {
-          minutes: 0,
-          seconds: 0
-        }
-      }
+
+    constructor(){
+      super();
+      this.state = {timeDisplay: {s: 0, m: 0}, seconds:10}
     }
 
+    componentDidMount() {
+      let timeLeft = this._secondsToTime(this.state.seconds);
+      this.setState({ timeDisplay: timeLeft });
+    }
+    _secondsToTime(sec){
+      let minutes = this._minTwoDigits(Math.floor(sec / 60));
+      let seconds = this._minTwoDigits(sec%60);
+      let obj = {
+        "m": minutes,
+        "s": seconds
+      }
+      return obj;
+
+    }
 
     _minTwoDigits(n) {
       return (n < 10 ? '0' : '') + n;
     };
     _runTimer(){
       const { fromNum, toNum} = this.props;
-      let actualNum = fromNum;
       let timer = setInterval(() => {
-        if(actualNum>toNum){
-          actualNum--;
-          let minutes = this._minTwoDigits(Math.floor(actualNum / 60));
-          let seconds = this._minTwoDigits(actualNum%60);
-          console.log(timeDisplay);
+        if(this.state.seconds>toNum){
+          let seconds = this.state.seconds - 1;
           this.setState({
-            timeDisplay: {
-              minutes: minutes,
-              seconds: seconds
-            }
-          });
-          console.log(`${this.timeDisplay.minutes}:${this.timeDisplay.seconds}`);
+            timeDisplay: this._secondsToTime(seconds),
+            seconds: seconds,
+          });    
+          console.log(this.state.timeDisplay);
+          console.log(`${this.state.timeDisplay.m}:${this.state.timeDisplay.s}`);
         }
       }, 1000);
     }
 
     render() {
-      this.getInitialState();
 
       this._runTimer();
 
       return (
         <div>
-          <h3>{this.state.timeDisplay.minutes}:{this.state.timeDisplay.seconds}</h3>
+          <h3>{this.state.timeDisplay.m}:{this.state.timeDisplay.s}</h3>
         </div>
       );
     }
