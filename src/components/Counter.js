2fc906794867eeccb1ad920c183dad93c473c633
Alicja Praczukowska <alicja.praczukowska@ctdi.com>
2018-04-23 11:16:04 +0200

    working

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,22 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class Counter extends React.Component {
-  
 
     static propTypes = {
       fromNum: PropTypes.number.isRequired,
       toNum: PropTypes.number.isRequired,
     }
 
-    constructor(){
-      super();
-      this.state = {timeDisplay: {s: 0, m: 0}, seconds:10}
+    state = {
+      timeDisplay: {s: 0, m: 0}, 
+      seconds: 0, 
+      timer: '', 
+      timerEnded: false,
+      timerStopped: false,
+      timerStarted: true
     }
 
     componentDidMount() {
-      let timeLeft = this._secondsToTime(this.state.seconds);
-      this.setState({ timeDisplay: timeLeft });
+      this.state.seconds = this.props.fromNum;
+      window.addEventListener('load', this._runTimer());
+      //let timeLeft = this._secondsToTime(this.state.seconds);
+      //this.setState({ timeDisplay: timeLeft });
     }
+
     _secondsToTime(sec){
       let minutes = this._minTwoDigits(Math.floor(sec / 60));
       let seconds = this._minTwoDigits(sec%60);
@@ -26,38 +32,73 @@ class Counter extends React.Component {
         "s": seconds
       }
       return obj;
-
     }
 
     _minTwoDigits(n) {
       return (n < 10 ? '0' : '') + n;
     };
+
     _runTimer(){
-      const { fromNum, toNum} = this.props;
-      let timer = setInterval(() => {
-        if(this.state.seconds>toNum){
-          let seconds = this.state.seconds - 1;
-          this.setState({
-            timeDisplay: this._secondsToTime(seconds),
+      let self = this;
+      const {toNum} = self.props;
+      let timerId = setInterval(() => {
+        console.log(self.state.seconds);
+        if(self.state.seconds>toNum){
+          let seconds = self.state.seconds - 1;
+          self.setState({
+            timeDisplay: self._secondsToTime(seconds),
             seconds: seconds,
-          });    
-          console.log(this.state.timeDisplay);
-          console.log(`${this.state.timeDisplay.m}:${this.state.timeDisplay.s}`);
+          });  
         }
+        else{
+				  self._handleTimerEnd();
+        } 
       }, 1000);
+      self.setState({
+        timer: timerId
+      });  
+    }
+
+    _handleTimerEnd(){
+      console.log('Timer Ended');
+      clearInterval(this.state.timer);
+      this.setState({
+        timerEnded: true,
+        timerStarted: false,
+        timerStopped: false
+      })
+    }
+
+    _handleTimerStopStart = event => {
+      if(this.state.timerStopped){
+        console.log('Timer Started');
+        this.setState({
+          timerEnded: false,
+          timerStarted: true,
+          timerStopped: false
+        })
+        this._runTimer();
+      }
+      else if(this.state.timerStarted){
+        console.log('Timer Stopped');
+        this.setState({
+          timerEnded: false,
+          timerStarted: false,
+          timerStopped: true
+        })
+        clearInterval(this.state.timer);
+      }
     }
 
     render() {
-
-      this._runTimer();
-
+      const {m, s} = this.state.timeDisplay;
       return (
         <div>
-          <h3>{this.state.timeDisplay.m}:{this.state.timeDisplay.s}</h3>
+          <h3 >{this.state.timeDisplay.m}:{this.state.timeDisplay.s}</h3>
+          <button onClick={this._handleTimerStopStart}>Stop/Start</button>
         </div>
       );
     }
-
   }
   
   export default Counter;
